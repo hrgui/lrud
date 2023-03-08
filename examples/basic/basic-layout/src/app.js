2f9debabf33cd7a946db1e84d41a97315cc0f04b
Harman Goei <5001432+hrgui@users.noreply.github.com>
2023-03-07 23:16:35 -0800

    poc: useLeafFocusedNode keeps changing, which causes dev listeners to unsub/sub - but if no movement happens then it won't unsub/sub

--- examples/basic/basic-layout/src/app.js
+++ examples/basic/basic-layout/src/app.js
@@ -1,30 +1,101 @@
-import { FocusNode } from '@please/lrud';
+import { FocusNode, useLeafFocusedNode } from '@please/lrud';
+import { useEffect } from 'react';
 import './app.css';
+
+// This maps a Key string, returned from an event, to a handler name.
+export const keyToBindingMap = {
+  ArrowUp: 'up',
+  ArrowDown: 'down',
+  ArrowLeft: 'left',
+  ArrowRight: 'right',
+};
+
+// This maps a KeyCode num value, returned from an event, to a handler name.
+export const keyCodeToBindingMap = {
+  38: keyToBindingMap.ArrowUp,
+  40: keyToBindingMap.ArrowDown,
+  37: keyToBindingMap.ArrowLeft,
+  39: keyToBindingMap.ArrowRight,
+};
+
+function useLRUD(mapping = {}) {
+  const keydownHandler = function (e) {
+    const bindingName =
+      keyToBindingMap[e.key] || keyCodeToBindingMap[e.keyCode];
+    const binding = mapping[bindingName];
+
+    if (typeof binding === 'function') {
+      e.preventDefault();
+      e.stopPropagation();
+
+      binding();
+    }
+  };
+
+  function subscribe() {
+    window.addEventListener('keydown', keydownHandler);
+  }
+
+  function unsubscribe() {
+    window.removeEventListener('keydown', keydownHandler);
+  }
+
+  useEffect(() => {
+    subscribe();
+    return () => unsubscribe();
+  });
+}
+
+function BoundaryListener() {
+  // if the leaf focused node changes
+  // then the LRUD events won't trigger
+  useLeafFocusedNode();
+
+  useLRUD({
+    up() {
+      console.log('up');
+    },
+    left() {
+      console.log('left');
+    },
+    right() {
+      console.log('right');
+    },
+    down() {
+      console.log('down');
+    },
+  });
+
+  return null;
+}
 
 export default function App() {
   return (
-    <FocusNode className="app">
-      <FocusNode
-        orientation="vertical"
-        className="block-container block-container-vertical">
-        <FocusNode className="block">One</FocusNode>
-        <FocusNode className="block">Two</FocusNode>
-        <FocusNode className="block">Three</FocusNode>
-      </FocusNode>
+    <>
+      <FocusNode className="app">
+        <FocusNode
+          orientation="vertical"
+          className="block-container block-container-vertical">
+          <FocusNode className="block">One</FocusNode>
+          <FocusNode className="block">Two</FocusNode>
+          <FocusNode className="block">Three</FocusNode>
+        </FocusNode>
 
-      <FocusNode
-        orientation="vertical"
-        className="block-container block-container-vertical">
-        <FocusNode className="block">One</FocusNode>
-        <FocusNode className="block">Two</FocusNode>
-        <FocusNode className="block">Three</FocusNode>
-      </FocusNode>
+        <FocusNode
+          orientation="vertical"
+          className="block-container block-container-vertical">
+          <FocusNode className="block">One</FocusNode>
+          <FocusNode className="block">Two</FocusNode>
+          <FocusNode className="block">Three</FocusNode>
+        </FocusNode>
 
-      <FocusNode className="block-container block-container-horizontal">
-        <FocusNode className="block">One</FocusNode>
-        <FocusNode className="block">Two</FocusNode>
-        <FocusNode className="block">Three</FocusNode>
+        <FocusNode className="block-container block-container-horizontal">
+          <FocusNode className="block">One</FocusNode>
+          <FocusNode className="block">Two</FocusNode>
+          <FocusNode className="block">Three</FocusNode>
+        </FocusNode>
       </FocusNode>
-    </FocusNode>
+      <BoundaryListener />
+    </>
   );
 }
